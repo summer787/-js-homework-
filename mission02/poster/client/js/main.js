a3a78aa0e29fa4f3a092292ffdc6f383d2a97011
leesy <leesy787@gmail.com>
2023-07-23 15:50:22 +0900

    handleSlider 함수 생성 및 수정

--- mission02/poster/client/js/main.js
+++ mission02/poster/client/js/main.js
@@ -1,84 +1,46 @@
 
-/* 썸네일 이미지를 클릭하면 메인 이미지와 배경이 바뀔 수 있도록 코드 로직을 작성해주세요. */
-
 const nav = getNode('nav');
-
-
+const ul = getNode("ul");
 
 // nav 클릭시 배경 색상 변경
 
-function setBgColor(event) {
-  const target = event.target.closest("li");
-  const index = attr(target,'data-index')
+function setBgColor(color) {
   const body = getNode("body")
-  const colorA= data[index-1].color[0];
-  const colorB = data[index-1].color[1];
+  const [colorA,colorB] = color;
   body.style.background = `linear-gradient(to bottom, ${colorA}, ${colorB})`;
 }
 
 // 이미지 변경
 
-function setImage(event) {
-  const target = event.target.closest("li");
-  const index = attr(target,'data-index')
-  const dataname = data[index-1].name.toLowerCase()
+function setImage(name,alt) {
   const visualImage = getNode('.visual img')
-  attr(visualImage,'src',`./assets/${dataname}.jpeg`);
-  attr(visualImage,'alt',data[index-1].alt);
+  const lowername = name.toLowerCase()
+  attr(visualImage, "src", `./assets/${lowername}.jpeg`);
+  attr(visualImage, "alt", `./assets/${alt}.jpeg`);;
 }
 
 // 텍스트 변경
 
-function setNameText(event) {
-  const target = event.target.closest("li");
+function setNameText(name) {
   const nickName = getNode(".nickName")
-  const index = attr(target,'data-index')
-  nickName.textContent = data[index-1].name;
+  nickName.textContent = name;
 }
-
-nav.addEventListener("click", handleClick);
-nav.addEventListener("click", setBgColor);
-nav.addEventListener("click", setImage);
-nav.addEventListener("click", setNameText);
-
 
 // 1. 클릭 이벤트 활성화
 
-function handleClick(event) {
-  const target = event.target.closest("li");
+function handleSlider(e) {
+  const target = e.target.closest("li");
   const list = [...ul.children];
-  list.forEach(li => removeClass(li,'is-active'));
-  addClass(target,'is-active')
+  if (!target) return;
+  list.forEach((li) => removeClass(li, "is-active"));
+  addClass(target, "is-active");
 
+  const index = attr(target,"data-index");
+  const realdata = index-1;
+  const { color, name, alt } = data[realdata];
+  
+  setBgColor(color);
+  setImage(name,alt);
+  setNameText(name);
 }
-
-
-function handleSlider(e){
-  e.preventDefault();
-  
-  const target = e.target.closest('li');
-  const anchor = e.target.closest('a');
-  
-  if(!target || !anchor) return;
-
-  
-  const list = [...navigation.children];
-
-  const index = attr(target,'data-index')
-
-  list.forEach(li => removeClass(li,'is-active'));
-
-
-  addClass(target,'is-active')
-
-
-
-
-
-
-}
-
-
-
-
-nav.addEventListener('click',handleSlider);
+nav.addEventListener("click", handleSlider);
